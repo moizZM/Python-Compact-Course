f01bb281604662279b9f1febf085d2180ffffb2a
Moiz Zaheer Malik <153621024+moizZM@users.noreply.github.com>
2026-09-16 18:56:01 +0200

    Complete Week 1 Tasks 2,3 and 4

---
+++ task2.py
@@ -0,0 +1,16 @@
+number = 10
+decimalnumber = 10.5
+text = "Hello, World!"
+wahrheitswert = True
+
+print("Integer:", number)
+print("Type:", type(number))
+
+print("Float:", decimalnumber)
+print("Type:", type(decimalnumber))
+
+print("String:", text)
+print("Type:", type(text))
+
+print("Boolean:", wahrheitswert)
+print("Type:", type(wahrheitswert))
---
+++ task3.py
@@ -0,0 +1,9 @@
+number = 10 
+decimal = 10.0
+text_number = "25"
+
+print(float(number))
+print(int(decimal))
+print(str(number))
+print(int(text_number))
+print(bool(number))
